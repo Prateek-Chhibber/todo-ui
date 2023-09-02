1f1f7787de772f09b76a5dbd3adfdc8b456b6187
prateekchhibber <0392pc@gmail.com>
2023-09-02 17:00:32 +0530

    Connect React App to Register REST API

--- src/components/RegisterComponent.jsx
+++ src/components/RegisterComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-// import { registerAPICall } from '../services/AuthService'
+import { registerAPICall } from '../services/AuthService'
 
 const RegisterComponent = () => {
 
