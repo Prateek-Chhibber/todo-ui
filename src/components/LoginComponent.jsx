fa98ef5f4b194677f4e6c0d3681a0b5ca3f321f3
prateekchhibber <0392pc@gmail.com>
2023-09-09 17:43:55 +0530

    Implement Role-Based Access Feature in React App

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -19,9 +19,12 @@ const LoginComponent = () => {
             // Basic Auth Token and storing in Local Storage
             // const token = 'Basic ' + window.btoa(username + ":" + password);
             const token = 'Bearer ' + response.data.accessToken;
+            
+            const role = response.data.role;
+
             storeToken(token);
 
-            saveLoggedInUser(username);
+            saveLoggedInUser(username,role);
             navigator("/todos")
 
             window.location.reload(false);
